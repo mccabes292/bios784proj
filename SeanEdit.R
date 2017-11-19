a5399ac41d5dc3ef9d01ae196d0af79e264564c1
mccabes <mccabes@live.unc.edu>
2017-11-19 18:01:25 -0500

    update

--- SeanEdit.R
+++ SeanEdit.R
@@ -1,14 +1,32 @@
 #library(BiocInstaller)
 #biocLite("curatedOvarianData")
+#biocLite("ConsensusClusterPlus")
+library(ConsensusClusterPlus)
 library(curatedOvarianData)
 library(Biobase)
+library(matrixStats)
 data(package="curatedOvarianData")
-data(TCGA_eset)
-head(TCGA_eset)
 
-data("GSE2109_eset")
-data("GSE26712_eset")
+data("GSE9891_eset")
 
-table(GSE2109_eset$sample_type)
+rM=colMeans(log2(t(exprs(GSE9891_eset)+1)))
+rVar=rowVars(log(exprs(GSE9891_eset)+1))
 
-table(GSE26712_eset$sample_type)
+
+#Grade, Stage (Summary vs. tumor), substage, age, tax, recurrence status, site of first recurrence,primary therapy outcome success
+#Debulking, % normal stromal tumor cells , batch
+
+
+outMeans=kmeans(t(exprs(GSE9891_eset)),centers=6)
+head(outMeans$size)
+
+
+outPC=prcomp(t(exprs(GSE9891_eset)))
+
+
+d=exprs(GSE9891_eset)
+
+results = ConsensusClusterPlus(d,maxK=6,reps=1000,pItem=0.8,pFeature=1,clusterAlg="hc",distance="pearson",seed=1262118388.71279,plot="png")
+table(results[[6]][["consensusClass"]])
+icl = calcICL(results)
+
