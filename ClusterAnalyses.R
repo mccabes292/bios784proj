2dafa8bb8f2c15e4b0ef964c0251adb410151bb6
Pooja Saha <psaha@live.unc.edu>
2017-12-01 15:45:31 -0500

    Commit to Rs tudio

---
+++ ClusterAnalyses.R
@@ -0,0 +1,13 @@
+load("finalSet")
+
+##PCA Plot - PC1 vs PC2, cluster by cluster membersip
+
+##Dendrogram - color by cluster
+
+##Heatmap - color
+
+##Descriptive Stats on cluster means
+
+##Regression on Covariates
+
+##Identify Genes??
