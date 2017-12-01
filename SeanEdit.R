6f6147b15977374eb47ae5c75d44d4854266309f
mccabes <mccabes@live.unc.edu>
2017-12-01 14:23:02 -0500

    edit 1

--- SeanEdit.R
+++ SeanEdit.R
@@ -8,13 +8,6 @@ library(Biobase)
 data(package="curatedOvarianData") #pulls all the datasets
 
 data(GSE9891_eset) #pulls data we are using
-head(GSE9891_eset)
-phenoData(GSE9891_eset)$sample_type
-table(GSE9891_eset$sample_type, GSE9891_eset$histological_type)
-
-head(exprs(GSE9891_eset))[,1:5] #view a small bit of the count data matrix
-
-colnames(phenoData(GSE9891_eset)) #all the clinical variables
 
 
 library(matrixStats)
@@ -24,7 +17,7 @@ library(dplyr)
 #gene filtering: the data was already log transformed
 #Genes with log expression values of <7 and a variance of
   #<0.5 were filtered out before clustering, leaving 8,732 probe sets
-rM=colMeans(t(exprs(GSE9891_eset)))
+rM=rowMeans((exprs(GSE9891_eset)))
 rVar=rowVars(exprs(GSE9891_eset))
 
 length(rM)
@@ -35,7 +28,7 @@ length(rVar)
 #Debulking, % normal stromal tumor cells , batch
 
 
-
+table(rM<7,rVar<0.5)
 
 
 #k Means Clustering Begins Here
@@ -45,23 +38,24 @@ d2 = d[which(rM>=7 | rVar >= 0.5),] #filtering out the genes
 dim(d2)
 
 #Run Consensus Cluster Plus to obtain consensus set
-results = ConsensusClusterPlus(d2,maxK=6,reps=1000,pItem=0.8,pFeature=0.8,clusterAlg="kmdist",distance="pearson",seed=1262118388.71279)
+results = ConsensusClusterPlus(d2,maxK=6,reps=1000,pItem=1,pFeature=1,clusterAlg="kmdist",distance="pearson",seed=12)
 table(results[[6]][["consensusClass"]])
 icl = calcICL(results)
 icl6=(icl$itemConsensus)[(icl$itemConsensus)$k==6,]
 
 icl6%>%group_by(item)%>%filter( itemConsensus==max(itemConsensus))->outICL6
-sum(outICL6$itemConsensus<0.8)
+sum(outICL6$itemConsensus>0.8)
 
 #Subset to consensus set
-consenSet=outICL6$item[outICL6$itemConsensus>0.8]
+#PROPOSAL: Subset to 70% instead
+consenSet=outICL6$item[outICL6$itemConsensus>0.7]
 #Obtain clusters for consensus set
-yClust=outICL6$cluster[outICL6$itemConsensus>0.8]
+yClust=outICL6$cluster[outICL6$itemConsensus>0.7]
 
 trainSet=d2[,consenSet]
 
 #Find NonConsensus Set
-nonConSet=outICL6$item[outICL6$itemConsensus<=0.8]
+nonConSet=outICL6$item[outICL6$itemConsensus<=0.7]
 testSet=d2[,nonConSet]
 
 #Run K Nearest Neighbor Algorithm
@@ -97,10 +91,12 @@ knnOut=knn(train=t(trainSet),test=t(testSet), cl=yClust,k=chosenK)
 
 
 library(HiDimDA)
-outLda=DACrossVal(t(trainSet),as.factor(yClust),TrainAlg = Dlda,kfold=2)
+outLda=Dlda(t(trainSet),as.factor(yClust),ldafun="classification")
+p1=predict(outLda,t(testSet))
 
-out1=Dlda(t(trainSet),as.factor(yClust))
-#outPC=prcomp(t(exprs(GSE9891_eset)))
+
+
+
 
 #Table 1 in paper: See how many of each sample type (LMP and Malignant) are in the cluster
 t=pData(GSE9891_eset)
